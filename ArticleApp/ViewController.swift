2ab3df3d4488be99387162a6d2a2e44b93803a31
Nicklaus Khaw <nicklaus.khaw@myfave.com>
2025-06-20 21:56:56 +0800

    Initial commit: Created iOS app template

---
+++ ArticleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArticleApp
+//
+//  Created by 80176 Nicklaus Khaw Liang Wei on 20/06/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ArticleAppTests/ArticleAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArticleAppTests.swift
+//  ArticleAppTests
+//
+//  Created by 80176 Nicklaus Khaw Liang Wei on 20/06/2025.
+//
+
+import Testing
+@testable import ArticleApp
+
+struct ArticleAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
